3e6aee17dfdb23dd2dc9a0801756264d860e522a
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2026-02-14 20:02:43 +0700

    setup Project

---
+++ Typescript - OOP/test/class.test.ts
@@ -0,0 +1,16 @@
+describe('Class', () => {
+    it('should can create class', () => {
+        class Customer {
+
+        }
+        
+        class Order {
+
+        }
+
+        const custom: Customer = new Customer();
+        const order = new Order();
+    });
+
+    
+});
